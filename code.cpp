fbc1f68905e7a3691f76b56fbe1adf6b15eb1c16
stefanbalta <stefan.balta.sb@gmail.com>
2022-03-21 12:36:10 -0400

    Joystick Detected and Program awaits Joystick

--- code.cpp
+++ code.cpp
@@ -7,19 +7,35 @@ void printCharacterOutput(const Uint8 state, const short& joystickState){
 int main(int argc, char* argv[]){
     SDL_Window* window = nullptr;
     SDL_Renderer* renderer = nullptr;
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+    SDL_Joystick* joystick = nullptr;
+    if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_JOYSTICK) < 0){
         std::cout << "SDL could not be initialized: " <<
                   SDL_GetError();
     }else{
         std::cout << "SDL video system is ready to go\n";
     }
-    /*
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    */
+    if(SDL_NumJoysticks()!=0){
+        SDL_JoystickEventState(SDL_ENABLE);
+        joystick = SDL_JoystickOpen(0);
+    }else{
+        std::cerr<<"Joystick Not Detected! Please plug it in"<<std::endl; 
+        short counter = 0;
+        while(true){
+            SDL_Delay(1000);
+            if(SDL_NumJoysticks()==0){
+                std::cerr<<"Joystick Not Detected!"<<std::endl;
+                if(counter == 100){
+                    SDL_Quit();
+                    return 0;
+                }
+                counter++;
+            }else{
+                SDL_JoystickEventState(SDL_ENABLE);
+                joystick = SDL_JoystickOpen(0);
+                break;
+            }
+        }
+    }
     window = SDL_CreateWindow("C++ SDL2 Window",
             0,
             0,
@@ -27,14 +43,8 @@ int main(int argc, char* argv[]){
             480,
             SDL_WINDOW_OPENGL);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    /*
-    SDL_GLContext context;
-    context = SDL_GL_CreateContext(window);
-    gladLoadGLLoader(SDL_GL_GetProcAddress);
-    */
     bool gameIsRunning = true;
     while(gameIsRunning){
-        //glViewport(0,0,640,480);
         SDL_Event event;
         while(SDL_PollEvent(&event)){
             const Uint8* state = SDL_GetKeyboardState(NULL);
@@ -51,11 +61,6 @@ int main(int argc, char* argv[]){
             SDL_RenderDrawLine(renderer,5,5,100,120);
             SDL_RenderPresent(renderer);
         }
-        /*
-        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-        SDL_GL_SwapWindow(window);
-        */
     }
     SDL_DestroyWindow(window);
     SDL_Delay(3000);
